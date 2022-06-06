d1e50bde70c11ba0c8cc076aafc75c64d34a9aff
Colin Higgins <colin-higgins@users.noreply.github.com>
2022-06-06 11:58:15 -0400

    Left out these changes somehow, explains the hang

--- observe.sh
+++ observe.sh
@@ -1,6 +1,64 @@
 
-SPAMMER_STATS_FILE=./results/${TRANSPORT}/spammer-stats.json
 
-echo "" > $SPAMMER_STATS_FILE
+#!/bin/bash
+# Description:  A wrapper script used to stop/start another script.
 
-echo -ne "GET /containers/${SPAMMER_CONTAINER_ID}/stats HTTP/1.1\r\n\r\n" >> $SPAMMER_STATS_FILE | sudo nc -U /var/run/docker.sock
+#--------------------------------------
+# Define Global Environment Settings:
+#--------------------------------------
+
+# Name and location of a persistent PID file
+
+PIDFILE="/observer-pid.txt"
+
+#--------------------------------------
+# Check command line option and run...
+# Note that "myscript" should not
+# provided by the user.
+#--------------------------------------
+
+case $1
+in
+    myscript)
+
+        SPAMMER_STATS_FILE=./results/${TRANSPORT}/spammer-stats.json
+        echo "" > $SPAMMER_STATS_FILE
+        echo -ne "GET /containers/${SPAMMER_CONTAINER_ID}/stats HTTP/1.1\r\n\r\n" >> $SPAMMER_STATS_FILE | sudo nc -U /var/run/docker.sock
+    ;;
+
+    start)
+        # Start your script in the background.
+        # (Note that this is a recursive call to the wrapper
+        #  itself that effectively runs your script located above.)
+        $0 myscript &
+
+        # Save the backgound job process number into a file.
+        jobs -p > $PIDFILE
+
+        # Disconnect the job from this shell.
+        # (Note that 'disown' command is only in the 'bash' shell.)
+        disown %1
+
+        # Print a message indicating the script has been started
+        echo "Script has been started..."
+    ;;
+
+    stop)
+        # Read the process number into the variable called PID
+        read PID < $PIDFILE
+
+        # Remove the PIDFILE
+        rm -f $PIDFILE
+
+        # Send a 'terminate' signal to process
+        kill $PID
+
+        # Print a message indicating the script has been stopped
+        echo "Script has been stopped..."
+    ;;
+
+    *)
+        # Print a "usage" message in case no arguments are supplied
+        echo "Usage: $0 start | stop"
+    ;;
+esac
