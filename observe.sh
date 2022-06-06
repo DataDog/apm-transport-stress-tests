a0892b1bbad2f88821496fce3e82012df4950a65
Colin Higgins <colin-higgins@users.noreply.github.com>
2022-06-06 12:21:44 -0400

    wrong ordering

--- observe.sh
+++ observe.sh
@@ -23,7 +23,7 @@ in
 
         SPAMMER_STATS_FILE=./results/${TRANSPORT}/spammer-stats.json
         echo "" > $SPAMMER_STATS_FILE
-        echo -ne "GET /containers/${SPAMMER_CONTAINER_ID}/stats HTTP/1.1\r\n\r\n" >> $SPAMMER_STATS_FILE | sudo nc -U /var/run/docker.sock
+        echo -ne "GET /containers/${SPAMMER_CONTAINER_ID}/stats HTTP/1.1\r\n\r\n" | sudo nc -U /var/run/docker.sock >> $SPAMMER_STATS_FILE
     ;;
 
     start)
