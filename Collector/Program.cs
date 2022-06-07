fe885a1118a20cd0147dcd1756de3eee3f5d91c9
Colin Higgins <colin-higgins@users.noreply.github.com>
2022-06-07 09:46:12 -0400

    Use mock agent, stub collector

---
+++ Collector/Program.cs
@@ -0,0 +1,7 @@
+﻿var start = DateTime.Now.Ticks;
+Console.WriteLine($"Beginning collector at {start}");
+
+// Scrub stats and send
+
+var end = DateTime.Now.Ticks;
+Console.WriteLine($"Ending orchestrator at {end}, a total of {end - start} ticks.");
