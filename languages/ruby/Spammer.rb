5fff8e54b877b4ac31e1a34087fba04a1cf9d9d9
Colin Higgins <colin-higgins@users.noreply.github.com>
2022-06-13 15:21:48 -0400

    Ruby sample

---
+++ languages/ruby/Spammer.rb
@@ -0,0 +1,18 @@
+require 'ddtrace/auto_instrument'
+
+Datadog.configure do |c|
+  # Add additional configuration here.
+end
+
+print "Starting spammer!\n"
+
+limit = 100000000
+ 
+while limit >= 1
+  Datadog::Tracing.trace('spam', resource: 'spammer') do |span|
+    Datadog::Tracing.trace('nested-spam') do
+      sleep(0.001)
+    end
+  end
+  limit = limit - 1
+end
