c74ecdfb65f44f1c149ed755476fe03375b8a684
Colin Higgins <colin-higgins@users.noreply.github.com>
2022-06-17 17:02:47 -0400

    remove sample rate again
    Increase concurrency to 1

--- languages/nodejs/index.js
+++ languages/nodejs/index.js
@@ -9,33 +9,18 @@ setTimeout(function() {
 
 console.log('Starting nodejs spammer.');
 
-var traceCount = 0;
-
-var maxRuns = 100000000;
-
-var sendLots = function() {
+var spam = function() {
     tracer.trace('spam', { resource: 'spammer' }, () => {
 		setTimeout(function() {	
             tracer.trace('nested-spam', {}, () => {})
 		}, 1);
     })
-	
-	traceCount++;
-	
-	if (traceCount % 1000 == 0) {
-		console.log('Another 1000 traces sent.')
-	}	
-	
-	if (--maxRuns <= 0) {
-		console.log('Exceeded maximum runs.');
-		return false;
-	}
-	
-	setTimeout(function() {	
-        sendLots();
-	}, 1);
 };
 
-sendLots();
+while (true) {
+	setTimeout(function() {	
+        spam();
+	}, 1);
+}
 
 console.log('Exiting nodejs spammer');
