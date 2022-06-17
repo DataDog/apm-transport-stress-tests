47cd6a45ba2fd898cd296c7073de27104da5deeb
Colin Higgins <colin-higgins@users.noreply.github.com>
2022-06-17 14:36:35 -0400

    show logs script

--- update-file-permissions.sh
+++ update-file-permissions.sh
@@ -4,3 +4,4 @@ git update-index --chmod=+x ./observe.sh
 git update-index --chmod=+x ./run.sh
 git update-index --chmod=+x ./build.sh
 git update-index --chmod=+x ./install-test-agent.sh
+git update-index --chmod=+x ./show-logs.sh
