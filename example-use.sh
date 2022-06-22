115cab68f4c05498941a301023cf14b3561a06e1
Colin Higgins <colin-higgins@users.noreply.github.com>
2022-06-22 16:57:37 -0400

    Update example-use.sh

--- example-use.sh
+++ example-use.sh
@@ -9,8 +9,10 @@ set -eu
 # DD_API_KEY must be available for the realagent
 # export DD_API_KEY=$YOUR_API_KEY
 
+# Set this environment variable to your tracer
+# export TRACER=dotnet #dotnet|java|python|golang|nodejs|ruby
+
 export AGENT_DOCKERFILE=realagent #realagent|mockagent|dotnetagent
-export TRACER=dotnet #dotnet|java|python|golang|nodejs|ruby
 
 # Associates the two separate runs
 export RUN_ID=$(date +%s)
@@ -20,6 +22,7 @@ export RUN_ID=$(date +%s)
 # export CONCURRENT_SPAMMERS=10
 
 git clone --depth 1 https://github.com/DataDog/apm-transport-stress-tests.git
+cd apm-transport-stress-tests
 ./build.sh $TRACER $AGENT_DOCKERFILE
 ./run.sh tcpip
 ./run.sh uds
