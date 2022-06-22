90e96f84d47e2ee8bd9f62f90eaf67d84e5cb03b
Bryan English <bryan.english@datadoghq.com>
2022-06-22 19:58:22 -0400

    more correct Node.js sample app

--- languages/nodejs/index.js
+++ languages/nodejs/index.js
@@ -1,26 +1,29 @@
 #!/usr/bin/env node
 
 const tracer = require('dd-trace').init();
+const { setTimeout } = require('timers/promises');
 
-console.log('Waiting for ready.');
-setTimeout(function() {
-    // no-op
-}, 10000);
+const oneMs = setTimeout.bind(null, 1);
 
-console.log('Starting nodejs spammer.');
-
-var spam = function() {
-    tracer.trace('spam', { resource: 'spammer' }, () => {
-		setTimeout(function() {	
-            tracer.trace('nested-spam', {}, () => {})
-		}, 1);
-    })
-};
-
-while (true) {
-	setTimeout(function() {	
-        spam();
-	}, 1);
+async function nestedSpam () {
+  return tracer.trace('nested-spam', {}, oneMs);
 }
 
-console.log('Exiting nodejs spammer');
+async function spam () {
+  await tracer.trace('spam', { resource: 'spammer' }, nestedSpam);
+}
+
+process.on('exit', () => {
+  console.log('Exiting Node.js spammer');
+});
+
+(async () => {
+  console.log('Waiting 10 seconds for agent to be ready');
+  await setTimeout(10000);
+
+  console.log('Starting Node.js spammer.');
+
+  while (true) {
+    await spam();
+  }
+})();
