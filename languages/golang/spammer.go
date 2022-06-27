2690a6cef7a0c0cbf34090b9659b63f826771845
Katie Hockman <katie.hockman@datadoghq.com>
2022-06-27 16:42:23 -0400

    Go implmentation sends events to DogStatsD

--- languages/golang/spammer.go
+++ languages/golang/spammer.go
@@ -2,18 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 
+	"github.com/DataDog/datadog-go/statsd"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
-
-func createTrace() {
-	span := tracer.StartSpan("spam", tracer.ResourceName("spammer"))
-	defer span.Finish()
-	childSpan := tracer.StartSpan("nested-spam", tracer.ChildOf(span.Context()))
-	time.Sleep(time.Millisecond)
-	childSpan.Finish()
-}
 
 func main() {
 	fmt.Printf("Waiting 10 seconds for agent to be ready\n")
@@ -21,10 +15,32 @@ func main() {
 	fmt.Printf("Starting spammer at: %v\n", time.Now().Unix())
 	tracer.Start()
 	defer tracer.Stop()
-
-	for {
-		createTrace()
+	statsdClient, err := statsd.New("127.0.0.1:8125")
+	if err != nil {
+		log.Fatal(err)
 	}
-
+	stressTest := &stressTest{statsdClient}
+	for {
+		stressTest.createTrace()
+	}
 	fmt.Printf("Finishing at: %v\n", time.Now().Unix())
+}
+
+type stressTest struct {
+	statsdClient *statsd.Client
+}
+
+// createTrace creates a trace with two spans,
+// where "nested-spam" is a child of "spam".
+// Each span creation sends an event to DogStatsD to
+// help with throughput analysis.
+func (st *stressTest) createTrace() {
+	st.statsdClient.SimpleEvent("transport_sample.span_created", "")
+	span := tracer.StartSpan("spam", tracer.ResourceName("spammer"))
+	defer span.Finish()
+
+	st.statsdClient.SimpleEvent("transport_sample.span_created", "")
+	childSpan := tracer.StartSpan("nested-spam", tracer.ChildOf(span.Context()))
+	time.Sleep(time.Millisecond)
+	childSpan.Finish()
 }
