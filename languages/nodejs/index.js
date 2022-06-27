a7a9d6594ecc0a837bf8c3cccb736c10c0454d10
sabrenner <sam.brenner@datadoghq.com>
2022-06-27 10:36:40 -0400

    adding statsd counter

--- languages/nodejs/index.js
+++ languages/nodejs/index.js
@@ -2,18 +2,31 @@
 
 const tracer = require('dd-trace').init();
 const { setTimeout } = require('timers/promises');
+const StatsD = require('hot-shots');
 
 const oneMs = setTimeout.bind(null, 1);
+const client = new StatsD({
+  port: 'observer:8125',
+  globalTags: { env: process.env.NODE_END },
+  errorHandler: function (error) {
+    console.log('Socket errors caught here:', error);
+  }
+});
 
 async function nestedSpam () {
+  client.increment('transport_sample.span_created');
   return tracer.trace('nested-spam', {}, oneMs);
 }
 
 async function spam () {
+  client.increment('transport_sample.span_created');
   await tracer.trace('spam', { resource: 'spammer' }, nestedSpam);
 }
 
 process.on('exit', () => {
+  client.close(function (error) {
+    console.log('Error closing StatsD', error)
+  });
   console.log('Exiting Node.js spammer');
 });
 
