ad57fe3a76bcc55f94b47a14444e1b3e30b99042
David Elner <david@davidelner.com>
2022-06-28 14:41:22 -0400

    Changed: Handle SIGINT and print results in Ruby spammer

--- languages/ruby/spammer.rb
+++ languages/ruby/spammer.rb
@@ -3,50 +3,90 @@ require 'ddtrace'
 
 class Spammer
   attr_reader \
-    :metrics
+    :metrics,
+    :results
 
   def initialize
     # Setup metrics
     @metrics = Datadog::Core::Metrics::Client.new
+    @results = {}
   end
 
   def run!
-    puts "[#{Time.now.utc}] Waiting ten seconds for agent to be ready\n"
-
-    sleep(10)
+    results[:traces_generated] = 0
+    results[:spans_generated] = 0
 
     puts "[#{Time.now.utc}] Starting spammer!\n"
 
     100000000.times do
       generate_trace!
     end
+
+    results
   end
 
   def generate_trace!
-    Datadog::Tracing.trace('spam', resource: 'spammer') do |span|
-      metrics.increment('transport_sample.span_created')
+    Datadog::Tracing.trace('spam', resource: 'spammer') do
       Datadog::Tracing.trace('nested-spam') do
         span_created(2)
         sleep(0.001)
       end
     end
+
+    trace_created
   end
 
-  def print_diagnostics!
-    puts "Tracing:\n- Enabled: #{Datadog.configuration.tracing.enabled}"
-    puts "Metrics:\n- Supported: #{metrics.supported?}\n- Enabled: #{metrics.enabled?}"
+  def print_setup!
+    puts "--------------"
+    puts "Spammer setup:"
+    puts "--------------"
+    puts "Tracing:"
+    puts "- Enabled:   #{Datadog.configuration.tracing.enabled}"
+    puts "Metrics:"
+    puts "- Supported: #{metrics.supported?}"
+    puts "- Enabled:   #{metrics.enabled?}"
+    puts "--------------"
+  end
+
+  def print_results!
+    puts "--------------"
+    puts "Spammer results:"
+    puts "--------------"
+    puts "Traces generated: #{results[:traces_generated]}"
+    puts "Spans generated:  #{results[:spans_generated]}"
+    puts "--------------"
+  end
+
+  private
+
+  def trace_created(count = 1)
+    results[:traces_generated] += count
   end
 
   def span_created(count = 1)
+    results[:spans_generated] += count
     metrics.count('transport_sample.span_created', count)
   end
 end
 
-# Run the spammer
 begin
+  # Create a spammer
   spammer = Spammer.new
-  spammer.print_diagnostics!
+  spammer.print_setup!
+
+  # Wait for agent to be ready
+  puts "[#{Time.now.utc}] Waiting ten seconds for agent to be ready\n"
+  sleep(ENV['DELAY_TIME'] || 10)
+
+  # Begin spamming
   spammer.run!
+
+  # Print spam results
+  puts "[#{Time.now.utc}] Spammer completed."
+  spammer.print_results!
+rescue Interrupt
+  puts "[#{Time.now.utc}] Spammer gracefully stopping..."
+  spammer.print_results!
 ensure
-  puts "[#{Time.now.utc}] Spammer shutting down!"
+  puts "[#{Time.now.utc}] Spammer exit."
 end
--- run.sh
+++ run.sh
@@ -168,7 +168,7 @@ docker-compose down --remove-orphans
 
 echo "Spammer exited with $EXIT_CODE, test will fail on non-zero."
 
-for unimplemented in golang nodejs java ruby
+for unimplemented in golang nodejs java
 do
 	if [[ "$TRACER" == "$unimplemented" ]]; then
 		echo "This language has not yet implemented graceful SIGINT"
