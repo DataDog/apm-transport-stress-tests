f96bf21d39ac35c6d08914f5847d0c43c6192d7a
Brett Langdon <me@brett.is>
2022-07-01 15:52:06 -0400

    python: add constant global tags (#17)
    * python: add constant global tags
    
    * Update languages/python/spammer.py
    
    * Send same metrics for python
    
    * Remove race condition by default
    
    Co-authored-by: Colin Higgins <colin.higgins@datadoghq.com>

--- languages/python/spammer.py
+++ languages/python/spammer.py
@@ -1,18 +1,30 @@
+import os
 import time
 
 from datadog import initialize, statsd
-from ddtrace import tracer
+from ddtrace import config, tracer
 
-options = {
-    "statsd_host": "observer",
-    "statsd_port": 8125,
-}
+# time.sleep(10)
 
-initialize(**options)
+initialize(
+    statsd_host="observer",
+    statsd_port=8125,
+    statsd_constant_tags=[
+        "language:python",
+        "transport:{}".format(os.environ["TRANSPORT"]),
+        "conc:{}".format(os.environ["CONCURRENT_SPAMMERS"]),
+        "trunid:{}".format(os.environ["TRANSPORT_RUN_ID"]),
+        "env:{}".format(os.environ["DD_ENV"]),
+        "service:{}".format(os.environ["DD_SERVICE"]),
+        "version:{}".format(os.environ["DD_VERSION"]),
+    ],
+)
 
 
 print("Starting spammer")
 spans_created = 0
+
+statsd.increment("transport_sample.run")
 
 while True:
     try:
@@ -30,5 +42,7 @@ while True:
         break
 
 
+statsd.increment("transport_sample.end")
+statsd.increment("transport_sample.span_logged", spans_created)
 print("Reached limit, exiting")
 print("Created {} spans".format(spans_created))
