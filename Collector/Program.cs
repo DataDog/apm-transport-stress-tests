16a455ab5bc3f5bfdfb246df06791372f7b27cbf
Colin Higgins <colin-higgins@users.noreply.github.com>
2022-07-21 09:45:01 -0400

    Unused experiments (#28)

--- Collector/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿var start = DateTime.Now.Ticks;
-Console.WriteLine($"Beginning collector at {start}");
-
-// Scrub stats and send
-
-var end = DateTime.Now.Ticks;
-Console.WriteLine($"Ending orchestrator at {end}, a total of {end - start} ticks.");
